1c3671f3036dc88870de202593c335faf6bf0cc2
mgwalkergit <mgwalker@andrew.cmu.edu>
2020-05-19 20:51:07 -0400

    Update m2fs_dark.py

--- m2fs_dark.py
+++ m2fs_dark.py
@@ -12,81 +12,70 @@ from astropy.modeling import models
 from ccdproc import Combiner
 import os
 import mycode
+import m2fs_process as m2fs
 matplotlib.use('TkAgg')
 
 directory='/nfs/nas-0-9/mgwalker.proj/m2fs/'
-#m2fsrun='nov18'
-#m2fsrun='jul15'
-m2fsrun='may19'
-#datadir='/nfs/nas-0-9/mgwalker.proj/m2fs/m2fs.astro.lsa.umich.edu/data/NovDec2018/'
-#datadir='/nfs/nas-0-9/mgwalker.proj/m2fs/m2fs.astro.lsa.umich.edu/data/Jul2015/'
-datadir='/nfs/nas-0-9/mgwalker.proj/m2fs/m2fs.astro.lsa.umich.edu/data/MayJun2019/'
+#m2fsrun='[enter_run_here]'
 
-with open(directory+m2fsrun+'_dark_raw') as f:
+with open(directory+'all_dark_raw') as f:
     data=f.readlines()[0:]
+m2fsrun=[]
 utdate=[]
 file1=[]
 file2=[]
 for line in data:
     p=line.split()
-    utdate.append(str(p[0]))
-    file1.append(int(p[1]))
-    file2.append(int(p[2]))
+    m2fsrun.append(str(p[0]))
+    utdate.append(str(p[1]))
+    file1.append(int(p[2]))
+    file2.append(int(p[3]))
+m2fsrun=np.array(m2fsrun)
 utdate=np.array(utdate)
 file1=np.array(file1)
 file2=np.array(file2)
 
 for ccd in (['b','r']):
     for chip in (['c1','c2','c3','c4']):
-        print(directory+m2fsrun+'_'+ccd+'_'+chip+'_master_bias.fits')
-        master_bias=astropy.nddata.CCDData.read(directory+m2fsrun+'_'+ccd+'_'+chip+'_master_bias.fits')
-        master_dark=[]
-        sig_master_dark=[]
+        debiased=[]
+        exptime=[]
+        master_debiased=[]
         master_exptime=[]
-        for i in range(0,len(utdate)):
-            debiased=[]
-            sig_debiased=[]
-            exptime=[]
+
+        for i in range(0,len(m2fsrun)):
+            print(directory+m2fsrun[i]+'_'+ccd+'_'+chip+'_master_bias.fits')
+            datadir=m2fs.get_datadir(m2fsrun[i])
+            master_bias=astropy.nddata.CCDData.read(directory+m2fsrun[i]+'_'+ccd+'_'+chip+'_master_bias.fits')
             for j in range(file1[i],file2[i]+1):
                 filename=datadir+utdate[i]+'/'+ccd+str(j).zfill(4)+chip+'.fits'
                 
                 data=astropy.nddata.CCDData.read(filename,unit=u.adu)#header is in data.meta
-                exptime.append(data.header['exptime'])
                 print(filename,data.header['object'],data.header['binning'])
-                data_with_deviation=ccdproc.create_deviation(data,gain=data.meta['egain']*u.electron/u.adu,readnoise=data.meta['enoise']*u.electron)
-                gain_corrected=ccdproc.gain_correct(data_with_deviation,data_with_deviation.meta['egain']*u.electron/u.adu,add_keyword={'gain_corr':'Done'})
-                cr_cleaned=ccdproc.cosmicray_lacosmic(gain_corrected,sigclip=5)
 
-                oscan_subtracted=ccdproc.subtract_overscan(cr_cleaned,overscan=cr_cleaned[:,1024:],overscan_axis=1,model=models.Polynomial1D(3),add_keyword={'oscan_corr':'Done'})
+                oscan_subtracted=ccdproc.subtract_overscan(data,overscan=data[:,1024:],overscan_axis=1,model=models.Polynomial1D(3),add_keyword={'oscan_corr':'Done'})
                 trimmed1=ccdproc.trim_image(oscan_subtracted[:,:1024],add_keyword={'trim1':'Done'})
                 trimmed2=ccdproc.trim_image(trimmed1[:1028,:1024],add_keyword={'trim2':'Done'})
-                debiased0=ccdproc.subtract_bias(trimmed2,master_bias)
-                debiased.append(debiased0)
-                sig_debiased.append(debiased0.uncertainty._array)
-            sig_debiased=np.array(sig_debiased)
-            exptime=np.array(exptime)
+                exptime.append(data.header['exptime'])
+                master_exptime.append(data.header['exptime'])
 
-            c=Combiner(debiased)
-            c.weights=1./sig_debiased**2
-            ccdall=c.average_combine(uncertainty_func=mycode.stdmean)
-            ccdall.header['exptime']=np.mean(exptime)
-            master_exptime.append(np.mean(exptime))
-            if np.std(exptime)>0.:
-                print('WARNING: subexposures for darks have different exposure times!!!!')
-                print(exptime)
-                np.pause()
-            master_dark.append(ccdall)
-            
-            sig_master_dark.append(ccdall.uncertainty._array)
-            ccdall.write(directory+m2fsrun+'_'+ccd+'_'+chip+'_master_dark'+str(i+1)+'.fits',overwrite=True)
-        sig_master_dark=np.array(sig_master_dark)
+                debiased0=ccdproc.subtract_bias(trimmed2,master_bias)
+                master_debiased.append(debiased0)
+
         master_exptime=np.array(master_exptime)
-        c=Combiner(master_dark)
-        c.weights=1./sig_master_dark**2
-        ccdall=c.average_combine(uncertainty_func=mycode.stdmean)
-        ccdall.header['exptime']=np.mean(master_exptime)
         if np.std(master_exptime)>0.:
             print('WARNING: subexposures for darks have different exposure times!!!!')
             print(master_exptime)
             np.pause()
-        ccdall.write(directory+m2fsrun+'_'+ccd+'_'+chip+'_master_dark.fits',overwrite=True)
+
+        c=Combiner(master_debiased)
+        c.clip_extrema(nlow=1,nhigh=1)
+        old_n_masked=0
+        new_n_masked=c.data_arr.mask.sum()
+        while (new_n_masked > old_n_masked):
+            c.sigma_clipping(low_thresh=3,high_thresh=3,func=np.ma.median)
+            old_n_masked=new_n_masked
+            new_n_masked=c.data_arr.mask.sum()
+
+        ccdall=c.average_combine()
+        ccdall.header['exptime']=np.mean(master_exptime)
+        ccdall.write(directory+ccd+'_'+chip+'_master_dark.fits',overwrite=True)
